085a81d13c818cc25b5d3ef69510f79a906fd75b
Sharifdesigns22 <shariftarver@gmail.com>
2021-06-17 10:25:51 -0700

    CRUD functionality with working sign in/out ability for users

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,13 +1,75 @@
 class LecturesController < ApplicationController
-  def new
+  before_action :set_lecture, only: %i[ show edit update destroy ]
+
+  # GET /lectures or /lectures.json
+  def index
+    @lectures = Lecture.all
   end
 
-  def create
-  end
-
-  def destroy
-  end
-
+  # GET /lectures/1 or /lectures/1.json
   def show
+    @lecture = Lecture.find(params[:id])
   end
+
+  # GET /lectures/new
+  def new
+    @lecture = Lecture.new
+  end
+
+  # GET /lectures/1/edit
+  def edit
+    @lecture = Lecture.find(params[:id])
+  end
+
+  # POST /lectures or /lectures.json
+  def create
+    @lecture = Lecture.new(lecture_params)
+    @lecture.save
+    respond_to do |format|
+      if @lecture.save
+        format.html { redirect_to @lecture, notice: "Lecture was successfully created." }
+        format.json { render :show, status: :created, location: @lecture }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @lecture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /lectures/1 or /lectures/1.json
+  def update
+    respond_to do |format|
+      if @lecture.update(lecture_params)
+        format.html { redirect_to @lecture, notice: "Lecture was successfully updated." }
+        format.json { render :show, status: :ok, location: @lecture }
+      else form
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @lecture.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /lectures/1 or /lectures/1.json
+  def destroy
+    @lecture.destroy
+    respond_to do |format|
+      format.html { redirect_to lectures_url, notice: "Lecture was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_lecture
+      @lecture = Lecture.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def lecture_params
+      params.require(:lecture).permit(:name, :classroom_number, :id)
+    end
+
+    # def self_columns
+    #   super.reject { |column| column.name  == 'teachers_id' || column.name == 'students_id'}
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   resources :lectures
   root 'lectures#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :users do 
+    match '/users/sign_in', to: "devise/sessions#new", via: 'get'
+    match '/users/sign_in', to: "devise/sessions#create", via: 'post'
+    match '/users/sign_out', to: "devise/sessions#destroy", via: 'get'
+  end
 end

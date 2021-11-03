1908b2d966b81d2e382c9149a4a3f562ba397650
Sharifdesigns22 <shariftarver@gmail.com>
2021-11-03 14:04:41 -0400

    Correcting devisr Application controller before_action issue with private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!, :configure_permitted_parameters
+    before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
     private
 

cf60ae49d8e765c864dfa04d973b8546709a99d9
Sharifdesigns22 <shariftarver@gmail.com>
2021-11-05 10:30:34 -0400

    fixing application error likely to do with added scope

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,7 +1,7 @@
 class Lecture < ApplicationRecord
     has_many :attendances, dependent: :destroy, foreign_key: :lectures_id
     validates :name, presence: true
-    scope :teachers_lectures, -> (user) { where(user.id: current_user.id) }
+    scope :teachers_lectures, -> (user) { where{user.id: current_user.id} }
 
 
     accepts_nested_attributes_for :attendances
